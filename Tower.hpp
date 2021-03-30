af3c991cb1cd708e8938d0b5f64e220e651093b8
Mike Litman <awesomefat@gmail.com>
2021-03-30 11:22:16 -0500

    changed stuff

---
+++ Tower.hpp
@@ -0,0 +1,15 @@
+#include "Disk.hpp"
+
+class Tower
+{
+    private:
+        Disk* top;
+        int count;
+
+    public:
+        Tower();
+        Disk* pop();
+        Disk* peek();
+        void push(Disk* d);
+        void display();
+};
